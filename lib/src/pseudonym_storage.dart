f04348419fd0383d2ec7858f5caa725acd4fa500
Fabian Moeckel <fabianmoeckel@gmx.de>
2024-08-27 12:12:18 +0200

    add documentation

--- lib/src/pseudonym_storage.dart
+++ lib/src/pseudonym_storage.dart
@@ -1,15 +1,27 @@
 import 'package:ad_hoc_ident/ad_hoc_ident.dart';
 
+/// Stores created pseudonym-[AdHocIdentity] mappings
+/// and ensures pseudonym uniqueness.
 abstract class PseudonymStorage {
+  /// Creates an in memory [PseudonymStorage].
   static MemoryPseudonymStorage memory() {
     return MemoryPseudonymStorage._internal();
   }
 
+  /// Returns the stored pseudonym of the [identity] or null if none is found.
   Future<String?> get(AdHocIdentity identity);
 
+  /// Tries to store a [pseudonym] for the [identity] and returns whether or not
+  /// the pseudonym was set.
+  ///
+  /// If the [identity] already has a stored pseudonym, the existing one is not
+  /// changed and false is returned. Otherwise returns true.
   Future<bool> set(AdHocIdentity identity, String pseudonym);
 }
 
+/// An in memory implementation of [PseudonymStorage].
+///
+/// Be aware that an in memory store is discarded when the app closes.
 class MemoryPseudonymStorage implements PseudonymStorage {
   final Set<String> _values = {};
   final Map<AdHocIdentity, String> _mappings = {};
@@ -31,6 +43,10 @@ class MemoryPseudonymStorage implements PseudonymStorage {
     return true;
   }
 
+  /// Removes the pseudonym of the [identity] from the store.
+  ///
+  /// Returns false, if there was no pseudonym and true if a pseudonym was
+  /// removed.
   Future<bool> remove(AdHocIdentity identity) async {
     final value = _mappings.remove(identity);
     if (value == null) {
@@ -40,6 +56,10 @@ class MemoryPseudonymStorage implements PseudonymStorage {
     return true;
   }
 
+  /// Removes the [pseudonym] from the store, if it exists for any identity.
+  ///
+  /// Returns false, if there was no such pseudonym and true if a pseudonym was
+  /// removed.
   Future<bool> removeByValue(String pseudonym) async {
     final removed = _values.remove(pseudonym);
     if (!removed) {
--- lib/src/word_pseudonym_encrypter.dart
+++ lib/src/word_pseudonym_encrypter.dart
@@ -4,6 +4,10 @@ import 'package:ad_hoc_ident/ad_hoc_ident.dart';
 import 'package:ad_hoc_ident_util_readable_pseudonym/ad_hoc_ident_util_readable_pseudonym.dart';
 import 'package:unique_names_generator/unique_names_generator.dart';
 
+/// Translates an AdHocIdentity's identifier to a human readable pseudonym.
+///
+/// Uses a [PseudonymStorage] to store created pseudonyms and respond with the
+/// same pseudonym for known [AdHocIdentity]s.
 class WordPseudonymEncrypter implements AdHocIdentityEncrypter {
   static final _defaultConfig = Config(
     dictionaries: [adjectives, colors, animals],
@@ -12,10 +16,25 @@ class WordPseudonymEncrypter implements AdHocIdentityEncrypter {
     style: Style.capital,
   );
 
+  /// The wrapped [AdHocIdentityEncrypter] providing a secure [AdHocIdentity].
   final AdHocIdentityEncrypter innerEncrypter;
   final UniqueNamesGenerator _generator;
+
+  /// The [PseudonymStorage] used to store created pseudonyms and retrieve
+  /// existing ones.
+  ///
+  /// If an [AdHocIdentity] is processed, a new pseudonym is created if it the
+  /// identity is not yet known. The newly created pseudonym is stored in the
+  /// [PseudonymStorage]. Before creating a new pseudonym, the storage is
+  /// checked for an existing pseudonym. If a pseudonym is found, it is
+  /// returned instead of creating a new one.
   final PseudonymStorage storage;
 
+  /// Creates a [WordPseudonymEncrypter] wrapping the [innerEncrypter].
+  ///
+  /// The [PseudonymStorage] is used to store created pseudonyms and retrieve
+  /// existing ones. The [generatorConfig] can be used to adjust the pseudonym
+  /// generator.
   WordPseudonymEncrypter(
       {required this.innerEncrypter,
       required this.storage,
